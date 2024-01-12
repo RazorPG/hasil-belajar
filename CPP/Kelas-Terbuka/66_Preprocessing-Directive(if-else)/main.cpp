852174cc53d32fd0a1e6096cf3a1e7c131140b21
RazorPG <rafidhilmi0504@gmail.com>
2024-01-12 21:21:09 +0700

    menambahkan hasil belajar tanggal 12-1-2024

---
+++ CPP/Kelas-Terbuka/66_Preprocessing-Directive(if-else)/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#define ID 1
+
+#if ID == 1
+#define LANG "Indonesia"
+#else
+#define LANG "English"
+#endif
+
+using namespace std;
+
+int main()
+{
+    cout << "bahasa dipilih: " << LANG << endl;
+
+    cin.get();
+    return 0;
+}
---
+++ CPP/Kelas-Terbuka/67_Preprocessing-Directive(include)/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "library.h"
+#include <string>
+
+#define PI 3.141596
+
+using namespace std;
+
+int main()
+{
+    cout << "berhasil" << endl;
+    cout << "PI : " << PI << endl;
+    cout << "KUADRAT: " << KUADRAT(5) << endl;
+    cout << "MAX : " << MAX(12, 5) << endl;
+    // cout << "STRING : " << data << endl;
+    cin.get();
+    return 0;
+}
